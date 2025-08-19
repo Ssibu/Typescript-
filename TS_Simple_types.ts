648630241b8390001bb34a5aa474f21c6b36d8e9
Ssibu <sibu25456@gmail.com>
2025-08-19 11:47:45 +0530

    .

--- TS_Simple_types.ts
+++ TS_Simple_types.ts
@@ -1,2 +1,13 @@
+/*There are three main primitives in JavaScript and TypeScript.
+
+boolean - true or false values
+number - whole numbers and floating point values
+string - text values like "TypeScript Rocks"
+There are also 2 less common primitives used in later versions of Javascript and TypeScript.
+
+bigint - whole numbers and floating point values, but allows larger negative and positive numbers than the number type.
+symbol are used to create a globally unique identifier.*/
 let firstname:string = "sibu";
 console.log(firstname)
+// let firstName: string = "Dylan"; // type string
+// firstName = 33; // attempts to re-assign the value to a different type
